e4ac033f90be7bdee14d7ed7576a856fa044954b
Omri Doron <omridoron3@gmail.com>
2018-01-23 10:17:14 +0200

    email validate

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, CardItem, Form, Item, Input, Label, Button, Text } from 'native-base';
-import FacebookLoginButton from'../components/FacebookLoginButton'
+import { Card, CardItem, Form, Content, Item, Input, Label, Button, Text } from 'native-base';
+import FacebookLoginButton from '../components/FacebookLoginButton'
+import { validateEmail } from '../utils'
 
 export default class LoginForm extends React.Component {
     constructor(props) {
@@ -18,52 +19,56 @@ export default class LoginForm extends React.Component {
     SignIn() {
         const email = this.state.email
         const password = this.state.password
-        this.props.sign(email, password)
+        // this.props.sign(email, password)
+        console.log('mail', validateEmail(email))
     }
     forgotPassword() {
         console.log('forgot')
     }
     facebook(user) {
         this.props.facebook(user)
-    
+
     }
     render() {
         return (
-            <Card>
-                <CardItem>
-                    <Form>
-                        <Item stackedLabel>
-                            <Label>Email</Label>
-                            <Input
-                                value={this.state.email}
-                                onChangeText={(email) => this.setState({ email })}
-                            />
-                        </Item>
-                        <Item stackedLabel last >
-                            <Label>Password</Label>
-                            <Input
-                                onChangeText={(password) => this.setState({ password })}
-                                value={this.state.password}
-                            />
-                        </Item>
-                        <Button style={{ margin: 10,marginBottom:0 }} block rounded onPress={() => this.SignIn()}>
-                            <Text>Sign In</Text>
+            <Content>
+                <Card>
+                    <CardItem>
+                        <Form>
+                            <Item stackedLabel>
+                                <Label>Email</Label>
+                                <Input
+                                    value={this.state.email}
+                                    onChangeText={(email) => this.setState({ email })}
+                                />
+                            </Item>
+                            <Item stackedLabel last >
+                                <Label>Password</Label>
+                                <Input
+                                    onChangeText={(password) => this.setState({ password })}
+                                    value={this.state.password}
+                                />
+                            </Item>
+                            <Button style={{ margin: 10, marginBottom: 0 }} block rounded onPress={() => this.SignIn()}>
+                                <Text>Sign In</Text>
+                            </Button>
+                        </Form>
+                    </CardItem>
+                    <CardItem style={{ flexDirection: 'column' }}>
+                        <Text note> or sign in with facebook </Text>
+                        <FacebookLoginButton facebook={(user) => this.facebook(user)} />
+                    </CardItem>
+                    <CardItem >
+                        <Button transparent warning onPress={() => this.forgotPassword()}>
+                            <Text>Forgot Password?</Text>
                         </Button>
-                    </Form>
-                </CardItem>
-                <CardItem style={{ flexDirection: 'column'}}>
-                    <Text note> or sign in with facebook </Text>
-                    <FacebookLoginButton facebook={(user)=>this.facebook(user)} />
-                </CardItem>
-                <CardItem >
-                    <Button transparent warning onPress={() => this.forgotPassword()}>
-                        <Text>Forgot Password?</Text>
-                    </Button>
-                    <Button transparent warning onPress={() => this.register()}>
-                        <Text>Register</Text>
-                    </Button>
-                </CardItem>
-            </Card>
+                        <Button transparent warning onPress={() => this.register()}>
+                            <Text>Register</Text>
+                        </Button>
+                    </CardItem>
+                </Card>
+            </Content>
+
         );
     }
 
--- src/utils.js
+++ src/utils.js
@@ -23,3 +23,8 @@ export const getLeagueGames = (games,schedule) =>{
   });
   return leagueGames
 }
+
+export const validateEmail =(email) => {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email.toLowerCase());
+}
